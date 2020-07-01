0aaa8ca38cb6ce0398bad38b66e921e5d85ec01c
brad toney <63473477+bravotango87@users.noreply.github.com>
2020-07-01 10:52:39 -0400

    added global routing class.

---
+++ ActionFilters/GlobalRouting.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Space.ActionFilters
+{
+    public class GlobalRouting : IActionFilter
+    {
+        private readonly ClaimsPrincipal _claimsPrincipal; 
+        public GlobalRouting(ClaimsPrincipal claimsPrincipal)
+        {
+            _claimsPrincipal = claimsPrincipal;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = context.RouteData.Values["controller"];
+            if (controller.Equals("Home"))
+            {
+                if (_claimsPrincipal.IsInRole("Customer"))
+                {
+                    context.Result = new RedirectToActionResult("Index",
+                        "Customers", null);
+                }
+            }
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+              
+        }
+    }
+}
