1fd1f537163e271c26ec27693b8a02ae41bc8d9e
brad toney <63473477+bravotango87@users.noreply.github.com>
2020-07-06 14:58:39 -0400

    added Itinerary model.

---
+++ Models/Itinerary.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Space.Models
+{
+    public class Itinerary
+    {
+        [Key]
+
+        public int ItineraryId { get; set; }
+
+        public string Destination { get; set; }
+
+        public string ThreeWeeksPrior { get; set; }
+
+        public string DayOfLaunch { get; set; }
+
+
+        public DateFormat Depature { get; set; }
+
+        public string RequiredForms { get; set; }
+
+        public string DayOne { get; set; }
+
+        public string WhatToDo { get; set; }
+
+       [ForeignKey("IdentityUser")]
+
+        public string IdentityUserId { get; set; }
+
+        public IdentityUser IdentityUser { get; set; }
+
+
+    }
+}
